0f528214a4d42eada86665a0042063dde0013ba9
venegator <m.venega94@gmail.com>
2016-01-07 15:11:09 +0100

    añadida la vista crear_artista

--- application/views/crear_artista.php
+++ application/views/crear_artista.php
@@ -33,7 +33,7 @@
       
       <header class="header dark-bg">
             <!--logo start-->
-            <a class="logo">Administrador</a>
+            <a class="logo">Crear artista</a>
             <!--logo end-->
       </header>      
       <!--header end-->
@@ -63,6 +63,74 @@
           </div>
       </aside>
       <!--sidebar end-->
+
+      <!--main content start-->
+      <section id="main-content">
+        <section class="wrapper">
+          <section class="panel">
+            <div class="row">
+              <div class="col-lg-12">
+                <h3 class="page-header"><i class="glyphicon glyphicon-user" aria-hidden="true"></i> Crear artista</h3>
+                  <ol class="breadcrumb">
+                    <li><i class="fa fa-home"></i>Administrador</li>
+                    <li><i class="glyphicon glyphicon-user" aria-hidden="true"></i>Crear artista</li>
+                  </ol>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-lg-12">
+                <section class="panel">
+                  <header class="panel-heading">
+                    Crear artista
+                  </header>
+                  <div class="panel-body">
+                    <form class="form-horizontal " method="get">
+                      <div class="form-group">
+                        <label class="col-sm-2 control-label">Nombre <span class="required">*</span></label>
+                          <div class="col-sm-10">
+                            <input type="text" id="cnombre" name="nombre" class="form-control" required>
+                          </div>
+                      </div>
+                      <div class="form-group">
+                          <label for="insertarImagen">Imagen</label>
+                          <input type="file" id="insertarImagen">
+                          <p class="help-block">Inserte la imagen del artista.</p>
+                      </div>
+                      <div class="form-group ">
+                          <label for="descripcion" class="control-label col-lg-2">Descripción <span class="required">*</span></label>
+                          <div class="col-lg-10">
+                              <textarea class="form-control " id="cdescripcion" name="descripcion" required></textarea>
+                          </div>
+                      </div>
+                      <div class="form-group">
+                          <div class="col-lg-offset-2 col-lg-10">
+                              <button class="btn btn-primary" type="submit">Save</button>
+                              <button class="btn btn-default" type="button">Cancel</button>
+                          </div>
+                      </div>
+
+                    </form>
+                  </div>
+                </section>
+              </div>
+            </div>
+          </section>
+        </section>
+      </section>
+      <!--main content end-->
   </section>
+  <!-- container section end-->
+  <!-- javascripts -->
+  <script src="js/jquery.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <!-- jquery validate js -->
+  <script type="text/javascript" src="js/jquery.validate.min.js"></script>
+
+  <!-- custom form validation script for this page-->
+  <script src="js/form-validation-script.js"></script>
+  <!--custome script for all page-->
+  <script src="js/scripts.js"></script>
+
   </body>
 </html>
---
+++ application/views/layout/admin_view.php
@@ -0,0 +1,177 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Creative - Bootstrap 3 Responsive Admin Template">
+    <meta name="author" content="GeeksLabs">
+    <meta name="keyword" content="Creative, Dashboard, Admin, Template, Theme, Bootstrap, Responsive, Retina, Minimal">
+    <link rel="shortcut icon" href="img/favicon.png">
+
+    <title>Administrador</title>
+
+    <!-- Bootstrap CSS -->    
+   <link href="<?php echo base_url("assets/css/bootstrap.min.css"); ?>" rel="stylesheet">
+    <link href="<?php echo base_url("assets/css/styles.css"); ?>" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    <!-- bootstrap theme -->
+    <link href="<?php echo base_url("assets/css/bootstrap-theme.css"); ?>" rel="stylesheet">
+    <!-- font icon -->
+    <link href="<?php echo base_url("assets/css/elegant-icons-style.css"); ?>" rel="stylesheet" />
+    <link href="<?php echo base_url("assets/css/font-awesome.min.css"); ?>" rel="stylesheet" />    
+  	<link href="css/widgets.css" rel="stylesheet">
+    <link href="<?php echo base_url("assets/css/style.css"); ?>" rel="stylesheet">
+    <link href="<?php echo base_url("assets/css/style-responsive.css"); ?>" rel="stylesheet" />
+  	<link href="css/jquery-ui-1.10.4.min.css" rel="stylesheet">
+    
+  </head>
+
+  <body>
+  <!-- container section start -->
+  <section id="container" class="">
+     
+      
+      <header class="header dark-bg">
+            <!--logo start-->
+            <a class="logo">Administrador</a>
+            <!--logo end-->
+      </header>      
+      <!--header end-->
+
+      <!--sidebar start-->
+      <aside>
+          <div id="sidebar"  class="nav-collapse ">
+              <!-- sidebar menu start-->
+              <ul class="sidebar-menu">                
+                  <li>
+                      <a href="<?= base_url()?>crear_artista">
+                          <span class="glyphicon glyphicon-user" aria-hidden="true"> Añadir artista</span>
+                      </a>
+                  </li>
+				          <li>
+                      <a href="#">
+                          <span class="glyphicon glyphicon-music" aria-hidden="true"> Añadir canción</span>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="#">
+                          <span class="glyphicon glyphicon-plus" aria-hidden="true"> Añadir sello</span>
+                      </a>
+                  </li>
+              </ul>
+              <!-- sidebar menu end-->
+          </div>
+      </aside>
+      <!--sidebar end-->
+      
+      <!--main content start-->
+      <section id="main-content">
+          <section class="wrapper">            
+              <!--overview start-->
+			  <div class="row">
+				<div class="col-lg-12">
+					<h3 class="page-header" style="text-align: center">Página principal del administrador</h3>
+                    <p style="text-align: center">Por favor seleccione una opción del panel lateral.</p>
+				</div>
+			  </div>
+          </section>
+      </section>
+      <!--main content end-->
+
+  </section>
+
+  <!-- container section start -->
+
+    <!-- javascripts -->
+    <script src="js/jquery.js"></script>
+	  <script src="js/jquery-ui-1.10.4.min.js"></script>
+    <script src="js/jquery-1.8.3.min.js"></script>
+    <script type="text/javascript" src="js/jquery-ui-1.9.2.custom.min.js"></script>
+    <!-- bootstrap -->
+    <!-- <script src="js/bootstrap.min.js"></script> -->
+    <!-- nice scroll -->
+    <!--<script src="js/jquery.scrollTo.min.js"></script>-->
+    <!--<script src="js/jquery.nicescroll.js" type="text/javascript"></script>-->
+    <!-- charts scripts -->
+    <!--<script src="assets/jquery-knob/js/jquery.knob.js"></script>-->
+    <!--<script src="js/jquery.sparkline.js" type="text/javascript"></script>-->
+    <!--<script src="assets/jquery-easy-pie-chart/jquery.easy-pie-chart.js"></script>-->
+    <!--<script src="js/owl.carousel.js" ></script>
+
+    <<script src="js/fullcalendar.min.js"></script> 
+	<script src="assets/fullcalendar/fullcalendar/fullcalendar.js"></script>
+
+    <script src="js/calendar-custom.js"></script>
+	<script src="js/jquery.rateit.min.js"></script>
+
+    <script src="js/jquery.customSelect.min.js" ></script>
+	<script src="assets/chart-master/Chart.js"></script>-->
+   
+<!--
+    <script src="js/scripts.js"></script>
+
+    <script src="js/sparkline-chart.js"></script>
+    <script src="js/easy-pie-chart.js"></script>
+	<script src="js/jquery-jvectormap-1.2.2.min.js"></script>
+	<script src="js/jquery-jvectormap-world-mill-en.js"></script>
+	<script src="js/xcharts.min.js"></script>
+	<script src="js/jquery.autosize.min.js"></script>
+	<script src="js/jquery.placeholder.min.js"></script>
+	<script src="js/gdp-data.js"></script>	
+	<script src="js/morris.min.js"></script>
+	<script src="js/sparklines.js"></script>	
+	<script src="js/charts.js"></script>
+	<script src="js/jquery.slimscroll.min.js"></script>-->
+  <script>
+
+ //      //knob
+ //      $(function() {
+ //        $(".knob").knob({
+ //          'draw' : function () { 
+ //            $(this.i).val(this.cv + '%')
+ //          }
+ //        })
+ //      });
+
+ //      //carousel
+ //      $(document).ready(function() {
+ //          $("#owl-slider").owlCarousel({
+ //              navigation : true,
+ //              slideSpeed : 300,
+ //              paginationSpeed : 400,
+ //              singleItem : true
+
+ //          });
+ //      });
+
+ //      //custom select box
+
+ //      $(function(){
+ //          $('select.styled').customSelect();
+ //      });
+	  
+	//   /* ---------- Map ---------- */
+	// $(function(){
+	//   $('#map').vectorMap({
+	//     map: 'world_mill_en',
+	//     series: {
+	//       regions: [{
+	//         values: gdpData,
+	//         scale: ['#000', '#000'],
+	//         normalizeFunction: 'polynomial'
+	//       }]
+	//     },
+	// 	backgroundColor: '#eef3f7',
+	//     onLabelShow: function(e, el, code){
+	//       el.html(el.html()+' (GDP - '+gdpData[code]+')');
+	//     }
+	//   });
+	// });
+
+
+
+  </script>
+
+  </body>
+</html>
